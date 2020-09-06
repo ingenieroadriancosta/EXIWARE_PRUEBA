71c438ecd2b7d156b04fc47f545d713f5b416108
adrian <adrianjocos@hotmail.com>
2020-09-05 22:16:53 -0500

    1.0

--- CMDs.cmd
+++ CMDs.cmd
@@ -1,4 +1,11 @@
 CALL git push -u origin master
 pause
 
+exit
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/ingenieroadriancosta/EXIWARE_PRUEBA.git
+git push -u origin master
 
